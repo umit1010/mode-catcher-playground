ca38e4e7ba9c6e6c7b1ecd48c28bf716bfedb105
Umit Aslan <umit@umitaslan.com>
2024-04-17 16:35:02 -0500

    removed the short demo txt file

--- app.py
+++ app.py
@@ -446,7 +446,7 @@ def display_knowledge_graph(
 
     # metrics plots
 
-    graph_metrics = "Nothing to display yet!"
+    graph_metrics = "This section is temporarily disabled!"
 
     # ave_clustering = nx.average_clustering(G) if len(node_clustering) > 0 else 0
 
@@ -533,7 +533,7 @@ if input_folder_path.is_dir():
         file_list.extend(text_files)
 
 input_file_dropdown = dbc.Select(
-    file_list, id="input-file-dropdown", value="00_demo.txt"
+    file_list, id="input-file-dropdown", value="01_cj2.txt"
 )
 
 mode_name_input = dbc.Input(id="mode-name", value="", placeholder="Enter mode name ...")
